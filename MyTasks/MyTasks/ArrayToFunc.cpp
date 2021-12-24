df520c9eb06e76e32947f7a43b58406341356520
Alexander <alexandersavchenko1993@gmail.com>
2021-12-24 22:26:54 +0200

    Added ArrayToFunc lesson

---
+++ MyTasks/MyTasks/ArrayToFunc.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//
+////Почему рандом одинаковый
+//void FillArray(int arr[], const  int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		arr[i] = rand() % 10;
+//	}
+//}
+//
+//void PrintArray(int arr[], const int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << arr[i] << endl;
+//	}
+//}
+//
+//
+//void main()
+//{
+//
+//	setlocale(LC_ALL, "ru");
+//
+//	const int SIZE = 10;
+//
+//	int arr[SIZE];
+//	FillArray(arr, SIZE);
+//	PrintArray(arr, SIZE);
+//
+//
+//	cout << *(arr + 3) << endl;
+//}
